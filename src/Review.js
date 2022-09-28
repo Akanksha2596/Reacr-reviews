fb5a349ed7ddbfe50bb61860bd731f2ebb9ae9d3
Akanksha Yadav <programmingwithyadavakanksha.40@gmail.com>
2022-09-28 11:08:30 +0530

    reviews file modified

--- src/Review.js
+++ src/Review.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight,FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
     const[index,setIndex] = useState(0);
